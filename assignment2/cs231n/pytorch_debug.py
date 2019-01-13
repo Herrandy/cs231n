31653d44ec998d8adf7ad7b39038fa45f3f4e6a6
Antti Hietanen <antti.hietanen@tut.fi>
2019-01-13 15:41:56 +0200

    visualization

---
+++ assignment2/cs231n/pytorch_debug.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+import numpy as np
+
+NUM_TRAIN = 49000
+
+# The torchvision.transforms package provides tools for preprocessing data
+# and for performing data augmentation; here we set up a transform to
+# preprocess the data by subtracting the mean RGB value and dividing by the
+# standard deviation of each RGB value; we've hardcoded the mean and std.
+transform = T.Compose([
+                T.ToTensor(),
+                T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+            ])
+
+# We set up a Dataset object for each split (train / val / test); Datasets load
+# training examples one at a time, so we wrap each Dataset in a DataLoader which
+# iterates through the Dataset and forms minibatches. We divide the CIFAR-10
+# training set into train and val sets by passing a Sampler object to the
+# DataLoader telling how it should sample from the underlying Dataset.
+cifar10_train = dset.CIFAR10('./cs231n/datasets', train=True, download=True,
+                             transform=transform)
+loader_train = DataLoader(cifar10_train, batch_size=64,
+                          sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))
+
+cifar10_val = dset.CIFAR10('./cs231n/datasets', train=True, download=True,
+                           transform=transform)
+loader_val = DataLoader(cifar10_val, batch_size=64,
+                        sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN, 50000)))
+
+cifar10_test = dset.CIFAR10('./cs231n/datasets', train=False, download=True,
+                            transform=transform)
+loader_test = DataLoader(cifar10_test, batch_size=64)
+
+
+USE_GPU = True
+
+dtype = torch.float32 # we will be using float throughout this tutorial
+
+if USE_GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+# Constant to control how frequently we print train loss
+print_every = 100
+
+print('using device:', device)
+
+
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+def test_flatten():
+    x = torch.arange(12).view(2, 1, 3, 2)
+    print('Before flattening: ', x)
+    print('After flattening: ', flatten(x))
+
+test_flatten()
